c886d0da18e3f010c1e56f0ff8d938c5ed208047
yurnsoolora <lorajung@naver.com>
2023-06-07 15:26:07 +0900

    new2

--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -17,15 +17,21 @@ public class MemberHandler {
   static final char FEMALE = 'W';
 
   public static void inputMember() {
+    if (!available()) {
+      System.out.println("더이상 입력할 수 없습니다!");
+      return;
+    }
+
     name[length] = Prompt.inputString("이름? ");
     email[length] = Prompt.inputString("이메일? ");
     password[length] = Prompt.inputString("암호? ");
 
-    loop: while (true) {
-      String menuNo = Prompt.inputString("성별:\n" + 
-      "  1. 남자\n" + 
-      "  2. 여자\n" + 
-      "> ");
+    loop:
+    while (true) {
+      String menuNo = Prompt.inputString("성별:\n" +
+              "  1. 남자\n" +
+              "  2. 여자\n" +
+              "> ");
 
       switch (menuNo) {
         case "1":
@@ -49,11 +55,78 @@ public class MemberHandler {
     System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
-      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
+      System.out.printf("%d, %s, %s, %s\n",
+              no[i], name[i], email[i],
+              toGenderString(gender[i]));
     }
   }
 
-  public static boolean available() {
+  public static void viewMember() {
+    String memberNo = Prompt.inputString("번호? ");
+    for (int i = 0; i < length; i++) {
+      if (no[i] == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", name[i]);
+        System.out.printf("이메일: %s\n", email[i]);
+        System.out.printf("성별: %s\n", toGenderString(gender[i]));
+        return;
+      }
+    }
+    System.out.println("해당 번호의 회원이 없습니다!");
+  }
+
+  public static String toGenderString(char gender) {
+    return gender == 'M' ? "남성" : "여성";
+  }
+
+  private static boolean available() {
     return length < MAX_SIZE;
   }
+
+  public static void updateMember() {
+    String memberNo = Prompt.inputString("수정할 회원의 번호? ");
+    int index = findMemberIndex(memberNo);
+
+    if (index == -1) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
+    }
+
+    System.out.println("수정할 정보를 입력하세요. (변경하지 않을 항목은 엔터를 입력하세요)");
+
+    String newName = Prompt.inputString("이름? ");
+    if (!newName.equals("")) {
+      name[index] = newName;
+    }
+
+    String newEmail = Prompt.inputString("이메일? ");
+    if (!newEmail.equals("")) {
+      email[index] = newEmail;
+    }
+
+    String newPassword = Prompt.inputString("암호? ");
+    if (!newPassword.equals("")) {
+      password[index] = newPassword;
+    }
+
+    String newGender = Prompt.inputString("성별:\n" +
+            "  1. 남자\n" +
+            "  2. 여자\n" +
+            "> ");
+    if (!newGender.equals("")) {
+      gender[index] = newGender.toUpperCase().charAt(0);
+    }
+
+    System.out.println("회원 정보를 수정하였습니다.");
+  }
+
+  private static int findMemberIndex(String memberNo) {
+    int targetNo = Integer.parseInt(memberNo);
+    for (int i = 0; i < length; i++) {
+      if (no[i] == targetNo) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
+
--- myapp/app/src/main/java/bitcamp/util/Prompt.java
+++ myapp/app/src/main/java/bitcamp/util/Prompt.java
@@ -14,5 +14,4 @@ public class Prompt {
   public static void close() {
     scanner.close();
   }
-
 }
