dc6b3f355a666430304e18ec5b7a9dc38407b432
yurnsoolora <lorajung@naver.com>
2023-06-07 16:02:06 +0900

    newnew

--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -25,25 +25,9 @@ public class MemberHandler {
     name[length] = Prompt.inputString("이름? ");
     email[length] = Prompt.inputString("이메일? ");
     password[length] = Prompt.inputString("암호? ");
+    gender[length] = Prompt.inputString()
 
-    loop:
-    while (true) {
-      String menuNo = Prompt.inputString("성별:\n" +
-              "  1. 남자\n" +
-              "  2. 여자\n" +
-              "> ");
 
-      switch (menuNo) {
-        case "1":
-          gender[length] = MALE;
-          break loop;
-        case "2":
-          gender[length] = FEMALE;
-          break loop;
-        default:
-          System.out.println("무효한 번호입니다.");
-      }
-    }
 
     no[length] = userId++;
     length++;
@@ -83,50 +67,50 @@ public class MemberHandler {
   }
 
   public static void updateMember() {
-    String memberNo = Prompt.inputString("수정할 회원의 번호? ");
-    int index = findMemberIndex(memberNo);
-
-    if (index == -1) {
-      System.out.println("해당 번호의 회원이 없습니다!");
-      return;
-    }
-
-    System.out.println("수정할 정보를 입력하세요. (변경하지 않을 항목은 엔터를 입력하세요)");
-
-    String newName = Prompt.inputString("이름? ");
-    if (!newName.equals("")) {
-      name[index] = newName;
-    }
-
-    String newEmail = Prompt.inputString("이메일? ");
-    if (!newEmail.equals("")) {
-      email[index] = newEmail;
-    }
-
-    String newPassword = Prompt.inputString("암호? ");
-    if (!newPassword.equals("")) {
-      password[index] = newPassword;
-    }
-
-    String newGender = Prompt.inputString("성별:\n" +
-            "  1. 남자\n" +
-            "  2. 여자\n" +
-            "> ");
-    if (!newGender.equals("")) {
-      gender[index] = newGender.toUpperCase().charAt(0);
-    }
-
-    System.out.println("회원 정보를 수정하였습니다.");
-  }
-
-  private static int findMemberIndex(String memberNo) {
-    int targetNo = Integer.parseInt(memberNo);
+    String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
-      if (no[i] == targetNo) {
-        return i;
+      if (no[i] == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", name[i]);
+        name[i] = Prompt.inputString(name[i]);
+        System.out.printf("이메일: %s\n", email[i]);
+        email[i] = Prompt.inputString(email[i]);
+        System.out.printf("성별: %s\n", toGenderString(gender[i]));
+        gender[i] = inputGender(gender[i]);
+        return;
       }
     }
-    return -1;
+    System.out.println("해당 번호의 회원이 없습니다!");
   }
-}
+
+  private char inputGender(char default) {
+    String label;
+    if (default == 0) {
+      label = "성별:\n";
+    } else {
+      label = "성별(" + toGenderString(default) + ")?\n"
+    }
+
+    loop: while (true) {
+      String menuNo = Prompt.inputString(label +
+              "  1. 남자\n" +
+              "  2. 여자\n" +
+              "> ");
+
+      switch (menuNo) {
+        case "1":
+          return MALE;
+          break loop;
+        case "2":
+          return FEMALE;
+          break loop;
+        default:
+          System.out.println("무효한 번호입니다.");
+      }
+    }
+  }
+
+
+
+
+
 
